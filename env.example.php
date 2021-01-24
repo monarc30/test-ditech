d0ba796d94983e1c56396e25ebb571f9af55152b
Fernado Monarca <fernandomonarca@gmail.com>
2021-01-23 21:21:32 -0300

    criei o arquivo env.example.php

---
+++ env.example.php
@@ -0,0 +1,13 @@
+<?php
+
+putenv('DISPLAY_ERRORS_DETAILS='. false);
+
+$url = 'http://test-ditech';
+
+$url_api = $url . "/api/getdataGeneric.php";
+
+$host = 'localhost';
+$dbname = 'ditech';
+$user = 'root';
+$password = 'root';
+$port = '';
